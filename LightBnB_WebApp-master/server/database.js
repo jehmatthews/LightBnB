68692fbba60509bd9144c591e753fb440f3506bf
jehmatthews <jehmatthews30@gmail.com>
2022-11-07 12:43:26 -0800

    add package.json so dependencies are correct

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -3,6 +3,9 @@ const users = require('./json/users.json');
 const { Pool } = require('pg');
 
 const pool = new Pool({
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
   database: 'lightbnb'
 });
 
